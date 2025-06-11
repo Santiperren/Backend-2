0d6cd0c1ea2cb2fc46de947668e6b492e2f41f4a
Santi Perren <145618276+Santiperren@users.noreply.github.com>
2025-06-11 12:26:21 -0300

    arreglos

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+
+
+
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
+
+app.get("/", (req, res) => {
+  res.send("api node.js ");
+});
+
+app.post("/subir-imagen", upload.single("imagen"), (req, res) => {
+  const archivo = req.file;
+
+  if (!archivo) {
+    return res.status(400).json({ error: "No se subió ninguna imagen" });
+  }
+
+  
+  console.log("Recibido:", archivo.originalname);
+
+  res.json({
+    mensaje: "Imagen recibida correctamente",
+    nombre: archivo.originalname,
+    tipo: archivo.mimetype,
+    tamaño: archivo.size,
+  });
+});
+
+const port = process.env.port || 80;
+app.listen(port, () => console.log(`escuchando en puerto ${port}`));
+
+
