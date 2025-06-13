969d3dd5acaf8dada6d0f9a451284d3880ed062e
Santi Perren <145618276+Santiperren@users.noreply.github.com>
2025-06-13 15:30:18 -0300

    estoy perdido

--- routers/router.imagenes.js
+++ routers/router.imagenes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 
+
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
