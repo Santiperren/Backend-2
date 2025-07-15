9db6e79736a8e6909803bf91cd956aa66e70380d
Santi Perren <145618276+Santiperren@users.noreply.github.com>
2025-07-15 13:55:16 -0300

    cloudinary

--- upload.js
+++ upload.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: "<cloudname>",
-  api_key: "<api_key>",
-  api_secret: "<api_secret>"
+  cloud_name: "<djhu3ogof>",
+  api_key: "<118199274123919>",
+  api_secret: "<keE2l9uolytWlr86h7PXNGWjyO4>"
 });
 
 export default cloudinary;
