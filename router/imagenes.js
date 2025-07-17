080ef2a113f33f8f6e7b004ac71ec7e4b6119ea0
Santi Perren <145618276+Santiperren@users.noreply.github.com>
2025-07-17 15:53:27 -0300

    noc

--- router/imagenes.js
+++ router/imagenes.js
@@ -1,33 +1,12 @@
 import express from 'express';
 import multer from 'multer';
 
-
 const router = express.Router();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
+  
 
-const uploadDir = join(__dirname, "../uploads");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`)
-    }
-});
-const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-    if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(new Error('Invalid file type. Only PDF, PNG, JPEG, and JPG files are allowed.'), false);
-    }
-};
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter
-});
 router.post('/subir-imagen', upload.single('imagen'), (req, res) => {
   console.log('Archivo recibido:', req.file);
   const archivo = req.file;
